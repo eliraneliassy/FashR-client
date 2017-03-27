929d5088bfb05a2bdad27fd1a26be4cb042c92c3
Eliran Eliassy <eliran@stuccomedia.com>
2017-03-27 23:20:14 +0300

    migrate to cli 1.0.0

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { FeedService } from './../../services/feed-service.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, Inject } from '@angular/core';
+
 
 @Component({
   selector: 'fash-r-home',
@@ -11,11 +12,30 @@ export class HomeComponent implements OnInit {
   constructor(private feedService : FeedService) { }
 
   items: any =[];
+  page: number = 0;
+   max = (window.screen.height);
+
+  
+    
+@HostListener("window:scroll", ['$event'])
+onWindowScroll(event) {
+ console.log(event);
+ let pos = (document.documentElement.scrollTop || document.body.scrollTop);
+  
+   //console.log(pos);
+  
+ if(pos == this.max)   {
+  console.log(10);
+ }
+    
+    
+}
 
   ngOnInit() {
+     console.log(this.max);
     this.feedService.getFeed()
       .subscribe((res)=>{
-        debugger
+        //debugger
         this.items = res;
         console.log(this.items)
       })
